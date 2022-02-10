4ca23187cff261c01d24fcbcca7081b958df917e
Jennifer Cole <capauldi@gmail.com>
2022-02-10 03:48:56 -0800

    dotenv added

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root', // MySQL username
-        password: '', // MySQL password
-        database: 'employee_tracker'
+        user: process.env.DB_USER, // MySQL username
+        password: process.env.DB_PASSWORD, // MySQL password
+        database: process.env.DB_NAME
     },
     console.log('Connected to the employee_tracker database.')
 );
